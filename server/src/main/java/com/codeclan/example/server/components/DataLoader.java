244342a00de1a9c67ea29d0c29437b1e6b105d72
Nikita Schultz <nikita.schultz1996@gmail.com>
2020-08-06 18:36:00 +0100

    set up dataloader and tested travellerrepository

--- server/src/main/java/com/codeclan/example/server/components/DataLoader.java
+++ server/src/main/java/com/codeclan/example/server/components/DataLoader.java
@@ -1,4 +1,23 @@
 package com.codeclan.example.server.components;
 
-public class DataLoader {
+import com.codeclan.example.server.models.Traveller;
+import com.codeclan.example.server.repositories.TravellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    TravellerRepository travellerRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Traveller traveller = new Traveller("Nikita", "beach");
+        travellerRepository.save(traveller);
+
+        
+    }
 }
