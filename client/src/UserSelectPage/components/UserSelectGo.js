8c9b6f69280cb7ddc45a5dc6767f76299f699364
samcassie <samgcassie@gmail.com>
2020-08-07 00:54:55 +0100

    user select page basic props and layout set up

---
+++ client/src/UserSelectPage/components/UserSelectGo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserSelectGo = () => {
+
+    return (
+        <button className="goButton" type="button"> Enter </button>
+    )
+
+}
+
+export default UserSelectGo;
