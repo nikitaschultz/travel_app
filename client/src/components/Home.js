a7c84c22cb2b2f05095886a4b020ae69e74a72af
Hitanbot <matthewjamesworkmail@gmail.com>
2020-08-07 09:54:06 +0100

    updated stuff

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,10 @@
 import React, {Fragment} from 'react';
 import {Link} from 'react-router-dom';
 
-const Home = ({home}) => {
+const Home = (props) => {
 
 
-  const url = "/home";
+
 
   return (
 
