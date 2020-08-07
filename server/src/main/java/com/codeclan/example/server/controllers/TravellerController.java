89cbbbb8607d5f957c25188fee12a850b02e9bed
Nikita Schultz <nikita.schultz1996@gmail.com>
2020-08-07 13:10:02 +0100

    updated traveller controller

--- server/src/main/java/com/codeclan/example/server/controllers/TravellerController.java
+++ server/src/main/java/com/codeclan/example/server/controllers/TravellerController.java
@@ -2,11 +2,11 @@ package com.codeclan.example.server.controllers;
 
 import com.codeclan.example.server.models.Traveller;
 import com.codeclan.example.server.repositories.TravellerRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +18,30 @@ public class TravellerController {
     @GetMapping(value="/travellers")
     public ResponseEntity<List<Traveller>> getAllTravellers(){
         return new ResponseEntity<>(travellerRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping(value="/travellers")
+    public ResponseEntity<Traveller> postTraveller(
+            @RequestBody Traveller traveller
+    ){
+        travellerRepository.save(traveller);
+        return new ResponseEntity<>(traveller, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/travellers/{id}")
+    public ResponseEntity<Traveller> deleteTraveller(
+            @PathVariable Long id
+    ){
+        Traveller found = travellerRepository.getOne(id);
+        travellerRepository.delete(found);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @PatchMapping(value = "/travellers/{id}")
+    public ResponseEntity<Traveller> updateTraveller(
+            @RequestBody Traveller traveller
+    ){
+        travellerRepository.save(traveller);
+        return new ResponseEntity<>(traveller, HttpStatus.OK);
     }
 }
