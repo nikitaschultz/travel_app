21dd814779ac83ae520515f4c4deb73be5311aa5
Nikita Schultz <nikita.schultz1996@gmail.com>
2020-08-10 21:54:49 +0100

    added accommodation form

--- client/src/PlanContainer/components/AccommodationForm.js
+++ client/src/PlanContainer/components/AccommodationForm.js
@@ -1,10 +1,44 @@
 import React, { Component, Fragment } from 'react';
 
 class AccommodationForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      accommodation: {
+        planType: "ACCOMMODATION",
+        date: "",
+        bookingConfirmation: "",
+        name: "",
+        address: "",
+        numOfNights: 0
+      }
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+    let propertyName = event.target.name;
+    let accommodation = this.state.accommodation;
+    accommodation[propertyName] = event.target.value;
+    this.setState({accommodation: accommodation})
+  }
+
   render(){
     return (
       <Fragment>
-        <p>I'M THE ACCOMMODATION FORM</p>
+        <h4>Accommodation</h4>
+        <form>
+          <label htmlFor="date">Date:</label>
+          <input type="date" name="date" onChange={this.handleChange} value={this.state.accommodation.date} /><br />
+          <label htmlFor="bookingConfirmation">Booking Confirmation:</label>
+          <input type="text" name="bookingConfirmation" onChange={this.handleChange} value={this.state.accommodation.bookingConfirmation} /><br />
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" onChange={this.handleChange} value={this.state.accommodation.name} /><br />
+          <label htmlFor="address">Address:</label>
+          <input type="text" name="address" onChange={this.handleChange} value={this.state.accommodation.address} /><br />
+          <label htmlFor="numOfNights">Number of Nights:</label>
+          <input type="number" name="numOfNights" onChange={this.handleChange} value={this.state.accommodation.numOfNights} /><br />
+        </form>
       </Fragment>
     )
   }
