c0d7f629aeda6f09c46fdd16f69c56aa6cd7957e
Nikita Schultz <nikita.schultz1996@gmail.com>
2020-08-11 21:52:29 +0100

    redirections added for flights

--- client/src/PlanContainer/components/EventForm.js
+++ client/src/PlanContainer/components/EventForm.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import Confirmation from '../../MainContainer/components/Confirmation.js';
 
 class EventForm extends Component {
   constructor(props){
