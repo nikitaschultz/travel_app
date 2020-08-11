6d5dce73e3c00d570f7d1c5f48491e1fd8967e49
samcassie <samgcassie@gmail.com>
2020-08-11 22:12:38 +0100

    holidays home page

--- client/src/ProfileContainer/containers/ProfileContainer.js
+++ client/src/ProfileContainer/containers/ProfileContainer.js
@@ -1,11 +1,7 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
 import ProfileInfo from '../components/ProfileInfo.js';
 import ProfileEditPage from '../components/ProfileEditPage.js';
-<<<<<<< HEAD
-import ProfileNavBar from '../components/ProfileNavBar.js';
-=======
 import Request from '../../helpers/request.js';
->>>>>>> development
 
 class ProfileContainer extends Component{
     constructor(props){
@@ -43,23 +39,12 @@ class ProfileContainer extends Component{
 
     render(){
         return (
-<<<<<<< HEAD
-          <Fragment>
-            <div className="extended-container">
-              <ProfileNavBar selectedTraveller={this.props.selectedTraveller} logOut={this.props.logOut} />
-              <div className="container">
-                <ProfileEditPage onToggle={this.isEditingUser} isEditingUser={this.state.isEditingUser} profile={this.props.selectedTraveller} />
-                <ProfileInfo profile={this.props.selectedTraveller}/>
-              </div>
-=======
             <div className="extended-container">
                 <div className="container">
                     <ProfileEditPage onEditTraveller={this.handlePut} onToggle={this.isEditingUser} isEditingUser={this.state.isEditingUser} profile={this.props.selectedTraveller} />
                     <ProfileInfo onToggle={this.isEditingUser} profile={this.props.selectedTraveller}/>
                 </div>
->>>>>>> development
             </div>
-          </Fragment>
         )
     }
 
