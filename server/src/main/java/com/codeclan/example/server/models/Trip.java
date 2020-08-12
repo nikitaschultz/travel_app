e49a7d771cb132e898b0aac4b5b57d918d71d8d2
Nikita Schultz <nikita.schultz1996@gmail.com>
2020-08-12 11:51:57 +0100

    resolved final database issue

--- server/src/main/java/com/codeclan/example/server/models/Trip.java
+++ server/src/main/java/com/codeclan/example/server/models/Trip.java
@@ -29,7 +29,7 @@ public class Trip {
     @JoinColumn(name="holiday_id", nullable = false)
     private Holiday holiday;
 
-    @JsonIgnoreProperties(value="trip")
+    @JsonBackReference(value="plans")
     @OneToMany(mappedBy="trip", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Plan> plans;
 
